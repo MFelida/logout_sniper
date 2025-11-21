4ce0dd8c698adafc6e36f807d84112bd7cf51e0a
Mike Felida <mifelida@f0r3s1.codam.nl>
2025-11-21 15:42:06 +0100

    Definitely more, but better?

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,33 @@
 #!/usr/bin/env bash
 
+LOG_DIR="${HOME}/.local/state"
+LOG_NAME="logout_sniper.log"
+LOG="${LOG_DIR}/${LOG_NAME}"
+
+SCRIPT_DIR="${HOME}/.local/share"
+SCRIPT_NAME="logout_sniper.sh"
+SCRIPT="${SCRIPT_DIR}/${SCRIPT_NAME}"
+
+SERVICE_DIR="${HOME}/.config/systemd/user"
+SERVICE_NAME="logout_sniper.service"
+SERVICE="${SERVICE_DIR}/${SERVICE_NAME}"
+
+function log () {
+	function _log_stdin {
+		while read -r; do
+			echo "[$(date -Im)]" "$REPLY" | tee -a "${LOG}"
+		done
+	}
+	if [ $# -eq 0 ]; then
+		_log_stdin
+	else
+		echo "[$(date -Im)]" "$@" | tee -a "${LOG}"
+	fi
+}
+
 function make_service {
-	cat <<- EOF > ${HOME}/.config/systemd/user/logout_sniper.service
+	mkdir -p "${SERVICE_DIR}" |& log
+	cat <<- EOF > "${SERVICE}" 2> >(log)
 	[Unit]
 	Description="Logout when idle for too long"
 	After=gnome-session-x11-services-ready.target
@@ -9,57 +35,70 @@ function make_service {
 
 	[Service]
 	Environment=DISPLAY=${DISPLAY}
-	ExecStart=%h/.local/share/logout_sniper.sh --run
+	ExecStart=${SCRIPT} --run
 
 	[Install]
 	WantedBy=default.target
 	EOF
 }
 
-function log () {
-	local LOG_STRING="$(date -Im) $@"
-	echo $LOG_STRING
-	echo $LOG_STRING >> ${HOME}/.local/state/logout_sniper.log
-}
-
 function run {
-	if ! xprintidle > /dev/null 2>&1 ; then
-		xhost +local:
-		log "xprintidle failed"
-		while ! xprintidle > /dev/null 2>&1 ; do
-			sleep 5;
-		done
-		log "xprintidle success"
-	fi
+	function _lse_enabled {
+		local enable_file="/sgoinfre/mifelida_share/.lse"
+		builtin test -f "${enable_file}"
+		return $?
+	}
+	function _wait_xprintdidle_valid {
+		if ! xprintidle > /dev/null 2>&1 ; then
+			xhost +local:
+			log "xprintidle failed:" "$(xprintidle)"
+			while ! xprintidle > /dev/null 2>&1 ; do
+				sleep 5;
+			done
+			log "xprintidle success"
+		fi
+	}
 
 	MAX_IDLE_M=40
-	MAX_IDLE_MS="$(($MAX_IDLE_M * 60 * 1000))"
+	MAX_IDLE_MS="$((MAX_IDLE_M * 60 * 1000))"
 
 	while true; do
+		_wait_xprintdidle_valid
 		IDLE_TIME_MS=$(xprintidle)
-		if [ $IDLE_TIME_MS -gt $MAX_IDLE_MS ]; then
+		if [ "$IDLE_TIME_MS" -gt "$MAX_IDLE_MS" ]; then
 			log "Logout sniped"
-			if [ -f /sgoinfre/mifelida_share/.lse ]; then
+			if _lse_enabled; then
 				loginctl terminate-user $UID
 			fi
 			exit 0
 		else
-			SLEEP_TIME=$((($MAX_IDLE_MS - $(xprintidle)) / 1000 + 1))
-			sleep $SLEEP_TIME
+			SLEEP_TIME_S=$(((MAX_IDLE_MS - $(xprintidle)) / 1000 + 1))
+			sleep $SLEEP_TIME_S
 		fi
 	done
 }
 
 function install {
+	if [ -z "$DISPLAY" ]; then
+		echo "Error: DISPLAY not found in env. Service can't functon without it."
+		exit 1
+	fi
 	make_service;
-	cp -f "$0" "${HOME}/.local/share/logout_sniper.sh"
-	> "${HOME}/.local/state/logout_sniper.log"
+	if [[ "$_" == *INSTALL.sh ]] && [ -f "$_" ]; then
+		cp "$_" "${SCRIPT}" |& log
+	fi
+	if ! [ -f "${SCRIPT}" ]; then
+		echo "Script file not found. rtfm"
+	fi
+	chmod u+x "${SCRIPT}" | log
+	systemctl --user enable "${SERVICE_NAME}" | log
+	systemctl --user start "${SERVICE_NAME}" | log
 }
 
-if [ $# -lt 1 ]; then
-	install;
-	exit 0;
-elif [[ $1 == --run ]]; then
+if [[ $1 == --run ]]; then
 	run
-	exit 0;
+	exit 0
+else
+	install
+	exit 0
 fi
